31b1dc7d82229460e9ebfb21cde6487591094d11
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-08-23 17:10:43 +0300

    create outer loop structure

---
+++ loop.py
@@ -0,0 +1,8 @@
+Word='board'
+word_length=len(Word)
+Attempts=6
+
+for attempt in range(1,Attempts+1):
+   print(f'Attempt {attempt} out of {Attempts} attempts')
+   players_guess=input('Enter your guess : ')
+   
